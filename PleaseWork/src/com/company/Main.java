b3aaf49d8e675a84b179535e793e04fbc04328b8
tambov_nikita <tambovnikita@yandex.ru>
2021-02-19 16:56:39 +0300

    Добавил ввод данных
    Добавил ввод данных для фигур N и M.

--- PleaseWork/src/com/company/Main.java
+++ PleaseWork/src/com/company/Main.java
@@ -1,8 +1,57 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.print(5);
+        Scanner num = new Scanner(System.in);
+
+
+        // Данные для фигуры N
+        System.out.print("Введите кол-во точек фигуры N: ");
+        int count_tochek_N = num.nextInt();
+
+        int [] x_N = new int [count_tochek_N];
+        int [] H_N = new int [count_tochek_N];
+
+        System.out.println("Начните вводить координаты x, H точек:");
+        for (int i=0; i<count_tochek_N; i++) {
+            System.out.print("x" + (i+1) + " = ");
+            x_N[i] = num.nextInt();
+            System.out.print("H" + (i+1) + " = ");
+            H_N[i] = num.nextInt();
+        }
+
+        System.out.println("Координаты точек для фигуры N");
+
+        for (int i=0; i<count_tochek_N; i++) {
+            System.out.print(x_N[i] + ", ");
+            System.out.println(H_N[i]);
+        }
+
+
+        // Данные для фигуры M
+        System.out.print("Введите кол-во точек фигуры M: ");
+        int count_tochek_M = num.nextInt();
+
+        int [] x_M = new int [count_tochek_M];
+        int [] H_M = new int [count_tochek_M];
+
+        System.out.println("Начните вводить координаты x, H точек:");
+        for (int i=0; i<count_tochek_M; i++) {
+            System.out.print("x" + (i+1) + " = ");
+            x_M[i] = num.nextInt();
+            System.out.print("H" + (i+1) + " = ");
+            H_M[i] = num.nextInt();
+        }
+
+        System.out.println("Координаты точек для фигуры M");
+
+        for (int i=0; i<count_tochek_M; i++) {
+            System.out.print(x_M[i] + ", ");
+            System.out.println(H_M[i]);
+        }
+
     }
 }
